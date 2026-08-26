c77a7c29ed7763c83be8c7a633439dac1f032b2e
Krithi-NM <kruthinm1210@gmail.com>
2026-08-26 12:26:29 +0530

    Add: baseball-game - submission-0

---
+++ Data Structures & Algorithms/baseball-game/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector<int>scores;
+
+        for (int i=0; i<operations.size();i++){
+            if (operations[i]=="+"){
+                int n = scores.size();
+                scores.push_back(scores[n-1] + scores[n-2]);
+            }
+            else if (operations[i]=="D"){
+                scores.push_back(2 * scores.back());
+            }
+            else if (operations[i]=="C"){
+                scores.pop_back();
+            }
+            else {
+                scores.push_back(stoi(operations[i]));
+            }
+        }
+        int sum = 0;
+        for (int score : scores) {
+            sum += score;
+        }
+        return sum;
+    }
+};
