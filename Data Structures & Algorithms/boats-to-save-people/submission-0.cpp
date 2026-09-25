a0ca10455b4af50c69cf800b86dc9b69e6cf805f
Krithi-NM <kruthinm1210@gmail.com>
2026-09-25 19:27:35 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution { 
+public: 
+    int numRescueBoats(vector<int>& people, int limit) { 
+        int n=people.size(); 
+        int left=0; 
+        int right=n-1; 
+        int boat=0; 
+        
+        sort(people.begin(),people.end());
+        
+        while(left<=right){ 
+            if(people[left]+people[right]<=limit){ 
+                left++; 
+            } 
+            right--; 
+            boat++; 
+        }
+        return boat;
+    } 
+};
